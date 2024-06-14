f46e6e0d8924a5e2c1b7bc202edf0c103398e4ed
Abdul-Majid Nuhu <abdul-majid.nuhu@turntabl.io>
2024-06-14 07:25:42 +0000

    added delete route test

--- main.py
+++ main.py
@@ -4,24 +4,24 @@ from episode.http.httpstatus import HTTPStatus
 from episode.sqlmodel import Model, Session, DBType, DBConnection
 from episode.template_engine import render_template
 
-
 file_path = "student_db.sqlite"
 db_conn = DBConnection.dialect(DBType.SQLITE)
 connection = db_conn(database_path=file_path)
 
+
 class Student(Model):
     first_name: str
     last_name: str
-    user_name: str|None
-    age: int|None
+    user_name: str | None
+    age: int | None
 
 
 with Session(connection) as session:
     session.delete_and_create(Student)
 
-
 if __name__ == "__main__":
     episode = Episode()
+
 
     # Expecting a get request only with path parameter 'username'
     @episode.get("/students/{username}")
@@ -37,7 +37,7 @@ if __name__ == "__main__":
                     "last_name": student.last_name,
                     "age": student.age,
                 }
-                
+
                 return render_template("student.html", context=context)
             else:
                 return HttpResponse().write(
@@ -65,15 +65,37 @@ if __name__ == "__main__":
             "Data stored successfully", status_code=HTTPStatus.CREATED
         )
 
+
+    @episode.delete("/students/{username}")
+    def delete_student_by_username(request, username: str):
+        with Session(connection) as session:
+            sql_stmt = session.select(Student).where(Student.user_name == username)
+            students = list(session.exec(sql_stmt))
+            student = students[0] if students else None
+
+            if student:
+                session.delete(student)
+                return HttpResponse().write(
+                    "Student Deleted successfully", status_code=HTTPStatus.CREATED
+                )
+            else:
+                return HttpResponse().write(
+                    f"Student with username {username} does not exist",
+                    status_code=HTTPStatus.NOT_FOUND,
+                )
+
+
     # Expecting path parameter 'id' and a query parameter 'q'
     @episode.route("/data/{id}/")
-    def get_params(request, id:int, q: int):
+    def get_params(request, id: int, q: int):
         return HttpResponse().write({"id": id, "query_param": q})
+
 
     @episode.get("/index/")
     def get_index(request):
         return render_template(
             "index.html", context={"topics": ["Python", "Golang", "Java", "C++"]}
         )
+
 
     episode.start()
